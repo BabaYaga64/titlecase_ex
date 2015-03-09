b92c582ed53fe70bf8f564bdedd20250422527fc
Cynthia Wilhelm & Bojana Skarich <student@epicodus.com>
2015-03-09 13:56:49 -0700

    Add app file, src folder with TitleCaseGenerator.php, tests folder, TitleCaseGeneratorTest.php, views folder with twig files, and index.php.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -2,13 +2,28 @@
 
     class TitleCaseGenerator
     {
+        private $articles = array(
+            "a",
+            "and",
+            "the",
+            "in",
+            "of",
+            "or",
+            "on"
+        );
+
         function makeTitleCase($input_title)
         {
-            $input_array_of_words = explode(" ", $input_title);
+            $input_array_of_words = explode(" ", strtolower($input_title));
             $output_titlecased = array();
             foreach ($input_array_of_words as $word) {
-                array_push($output_titlecased, ucfirst($word));
-            }
+                if (in_array($word, $this->articles)) {
+                    array_push($output_titlecased, $word);
+                } else {
+                    array_push($output_titlecased, ucfirst($word));
+                };
+            };
+
             return implode(" ", $output_titlecased);
         }
     }
--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -17,7 +17,37 @@
         function test_makeTitleCase_multipleWords()
         {
             $test_TitleCaseGenerator = new TitleCaseGenerator;
-            $input = "the little mermaid";
+            $input = "black beauty";
+
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            $this->assertEquals("Black Beauty", $result);
+        }
+
+        function test_leaveLowerCase()
+        {
+            $test_LeaveLowerCase = new TitleCaseGenerator;
+            $input = 'beauty and the beast';
+
+            $result = $test_LeaveLowerCase->makeTitleCase($input);
+
+            $this->assertEquals("Beauty and the Beast", $result);
+        }
+
+        function test_makeCaseConsistent()
+        {
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = 'bEAuty AnD THE beasT';
+
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            $this->assertEquals("Beauty and the Beast", $result);
+        }
+
+        function test_makeTitleCase_firstWordIsArticle()
+        {
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = 'the Little Mermaid';
 
             $result = $test_TitleCaseGenerator->makeTitleCase($input);
 
