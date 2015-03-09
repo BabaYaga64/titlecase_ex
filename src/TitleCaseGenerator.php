590fa6a50d5374b25fde1abb331671e5124478d6
Cynthia Wilhelm & Bojana Skarich <student@epicodus.com>
2015-03-09 14:30:02 -0700

    Add new test for array for title capitalization.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -16,6 +16,8 @@
         {
             $input_array_of_words = explode(" ", strtolower($input_title));
             $output_titlecased = array();
+            array_push($output_titlecased, ucfirst($input_array_of_words[0]));
+            array_shift($input_array_of_words);
             foreach ($input_array_of_words as $word) {
                 if (in_array($word, $this->articles)) {
                     array_push($output_titlecased, $word);
